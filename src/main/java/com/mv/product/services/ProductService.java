3621cda3cbfa9b893adc828451c8c0bca85b79e9
Micael Vicente <micael.vicente@boost-it.pt>
2023-06-15 17:31:46 +0100

    feat: javadoc

--- src/main/java/com/mv/product/services/ProductService.java
+++ src/main/java/com/mv/product/services/ProductService.java
@@ -4,6 +4,13 @@ import java.util.UUID;
 
 public interface ProductService {
 
+  /**
+   * Retrieve a priced product given an sku and a quantity
+   *
+   * @param sku      the sku to look for
+   * @param quantity the quantity of SKU wanted
+   * @return the product with priced calculated
+   */
   Product getPricedProduct(UUID sku, int quantity);
 
 }
--- src/main/java/com/mv/product/services/pricing/PricingService.java
+++ src/main/java/com/mv/product/services/pricing/PricingService.java
@@ -4,5 +4,12 @@ import java.util.UUID;
 
 public interface PricingService {
 
+  /**
+   * Calculates the price of a product applying a discount if applicable.
+   *
+   * @param sku      the sku of the product
+   * @param quantity the quantity wanted
+   * @return the price components of this sku
+   */
   Price calculatePrice(UUID sku, int quantity);
 }
